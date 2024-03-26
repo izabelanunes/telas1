1e4b9f1ae3444e1f67c1462896420434b0cff341
izabela Nunes <izabela.nunes@estudante.ifms.edu.br>
2024-03-26 12:23:09 -0400

    aula2603

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {View, text} from "react- native";
+//todos os componetes usados, devem ter um import separado por virgula;
+
+
+function App(){
+     return(
+      //tem que ter para mostrar na tela
+
+      <View>
+        <Text>Olá Mundo!</Text>
+      </View>
+
+      //view é a tela ou o fragmento da tela
+
+     );
+}
+
+//depois de criar tenho que colocar o export
+export default App;//poderia por antes da function
+//indica a função que será executada 
+//o export não é o final do código
